3e78a1c5fea0098fd5d5b1fec4e51e9dfd1c65b6
harikrish-24 <haripriya.kk2501@gmail.com>
2025-06-22 20:12:56 -0400

    Add countOnly function with test

---
+++ countOnly.js
@@ -0,0 +1,36 @@
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+
+  return results;
+};
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result = countOnly(firstNames, {
+  "Jason": true,
+  "Karima": true,
+  "Fang": true
+});
+
+console.log(result);
