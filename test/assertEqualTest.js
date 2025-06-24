9263d153241c486d48ef89b2cbc1537cca7f7492
Hari Priya Krishnakumar <haripriya.kk2501@gmail.com>
2025-06-24 15:15:15 -0400

    Delete test directory

--- test/assertEqualTest.js
+++
@@ -1,6 +0,0 @@
-const assertEqual = require("../assertEqual");
-
-assertEqual("hello", "hello");  
-assertEqual(6, 6);              
-assertEqual("Priya", "Priya");  
-assertEqual(3, 8);              
