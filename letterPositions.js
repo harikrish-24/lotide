7b150c4068a54cf28ed8e389430993479e7de79d
harikrish-24 <haripriya.kk2501@gmail.com>
2025-06-25 22:44:06 -0400

    implement letterPostions with test cases

--- letterPositions.js
+++ letterPositions.js
@@ -1,3 +1,5 @@
+
+  //eqArrays function
 const eqArrays = function(arr1, arr2) {
     if (arr1.length !== arr2.length) return false;
     for (let i = 0; i < arr1.length; i++) {
@@ -5,7 +7,7 @@ const eqArrays = function(arr1, arr2) {
     }
     return true;
 };
-
+  //assertArraysEqual function
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
     console.log(`✅ Assertion Passed: [${arr1}] === [${arr2}]`);
@@ -13,7 +15,7 @@ const assertArraysEqual = function(arr1, arr2) {
     console.log(`🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
   }
 };
-
+  //added letterPositions function
 const letterPositions = function(sentence) {
     const results = {};
     for (let i = 0; i < sentence.length; i++) { 
